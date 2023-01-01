bf9b85225723aba075c3a780e181194406400be9
Your Name <you@example.com>
2023-01-01 12:20:23 +0100

    change file

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,14 +2,21 @@ import React, { useContext, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 
 const Budget = () => {
-  const { dispatch, budget } = useContext(AppContext)
+  const { dispatch, budget, expenses, currency } = useContext(AppContext)
   const [newbudget, setBudget] = useState(budget)
 
-  console.log(newbudget)
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost)
+  }, 0)
+
   function handleInputChange(event) {
     if (event.target.value > 20000) {
       alert('The budget cannot exceed the amount of 20000')
       setBudget('20000')
+      return
+    } else if (event.target.value < totalExpenses) {
+      alert('You cannot reduce the budget value lower the spanding')
+      setBudget(totalExpenses)
       return
     }
     setBudget(event.target.value)
@@ -21,16 +28,16 @@ const Budget = () => {
 
   return (
     <div className="alert alert-secondary">
-      <span>Budget: £</span>
+      <span>Budget: {currency}</span>
       <input
         required="required"
         type="number"
         id="budget"
         step={10}
         value={newbudget}
-        style={{ marginLeft: '2rem', size: 10 }}
+        style={{ width: '45%', marginLeft: '5px' }}
         onChange={(event) => handleInputChange(event)}
-      ></input>
+      />
     </div>
   )
 }
